714aceda2c43c0ab5a8317de0547fb37efa5eaab
Tim <sarrtim@gmail.com>
2017-12-27 20:32:43 +0300

    add model serializers

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -3,6 +3,12 @@ from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     class Meta:
         model = Category
         fields = ('name', 'slug',)
@@ -10,13 +16,40 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     class Meta:
         model = Product
         fields = ('category', 'name', 'slug', 'image', 'description', 'price', 'stock', 'available', 'created',
                   'updated',)
         fields = '__all__'
 
+
 class Order(serializers.Serializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     class Meta:
         model = Order
-        fields = ()
+        fields = ('user', 'order_total', 'created', 'updated', 'is_site_order', 'is_app_order',)
+        fields = '__all__'
+
+
+class OrderItem(serializers.Serializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+    class Meta:
+        model = Order
+        fields = ('order', 'product', 'price', 'quantity',)
+        fields = '__all__'

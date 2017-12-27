973a39c206d72927d880b2749e03da4ea6d1ea6b
Tim <sarrtim@gmail.com>
2017-12-27 20:32:23 +0300

    User on_delete

--- server/api/models.py
+++ server/api/models.py
@@ -39,7 +39,7 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_total = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)

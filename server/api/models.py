0c0d4ee201ae83b253050852e5bf38f0d1fbc2cc
Tim <sarrtim@gmail.com>
2017-12-28 23:38:48 +0300

    make product slug unique

--- server/api/models.py
+++ server/api/models.py
@@ -21,7 +21,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)

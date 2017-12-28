ccf809b8f3461077e49c2e7bed693b05cf4114ce
Tim <sarrtim@gmail.com>
2017-12-28 19:40:09 +0300

    add django rest swagger

--- server/app/settings.py
+++ server/app/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
     'rest_framework',
+    'rest_framework_swagger',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
--- server/app/urls.py
+++ server/app/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('api.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'', include('api.urls')),
 ]

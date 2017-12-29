662e0dd52f41fb7092c2d8e75924fa45d69a6e36
Tim <sarrtim@gmail.com>
2017-12-29 13:01:34 +0300

    add _models

---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,10 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    confirm_password: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+}

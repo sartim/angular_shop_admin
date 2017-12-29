df6ee869f59e30d4fc82a7be788e010e15d16b10
Tim <sarrtim@gmail.com>
2017-12-29 13:01:48 +0300

    add _services

---
+++ client/src/app/_services/authentication.service.ts
@@ -0,0 +1,67 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: Http) { }
+
+    login(username: string, password: string) {
+        let body = JSON.stringify({ username: username, password: password });
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        let login = this.http.post('http://127.0.0.1:8000/api/v1/auth/api-token-auth/', body, options)
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                let user = response.json();
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+                return user;
+            });
+
+        return login
+    }
+
+    refresh() {
+            let body = localStorage.getItem('currentUser');
+            let headers = new Headers({ 'Content-Type': 'application/json' });
+            let options = new RequestOptions({ headers: headers });
+
+            let refresh = this.http.post('http://127.0.0.1:8000/api/v1/auth/api-token-refresh/', body, options)
+                .map((response: Response) =>
+                {
+                    // login successful if there's a jwt token in the response
+                    let user = response.json();
+                    if (user && user.token) {
+                        // store user details and jwt token in local storage to keep user logged in between page refreshes
+                        localStorage.setItem('currentUser', JSON.stringify(user));
+                        console.log("updated token");
+                    }
+                    return user;
+                });
+
+            return refresh
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+        localStorage.removeItem('loggedUser');
+    }
+
+    getNewTokenHandler(){
+        setInterval(() => this.getNewToken(), 120000);
+    }
+
+    getNewToken() {
+        if (localStorage.getItem('currentUser')){
+            this.refresh().subscribe();
+        } else {
+            return false;
+        }
+    }
+}

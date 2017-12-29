f105665032ee8d15802eb4d687dfdbba5448ac17
Tim <sarrtim@gmail.com>
2017-12-29 13:01:14 +0300

    #for header

---
+++ client/src/app/header/header.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-header',
+    templateUrl: 'header.component.html',
+    styles: [`
+      input[type=text]:not(.browser-default){
+        background-color: #ffbd7a;
+      }
+      input.header-search-input:hover {
+          background: rgba(255,255,255,0.5);
+      }
+      input.header-search-input {
+          display: block;
+          padding: 8px 8px 8px 72px;
+          width: 100%;
+          background: rgba(255,255,255,0.3);
+          height: 24px;
+          -webkit-transition: all 200ms ease;
+          transition: all 200ms ease;
+          border: none;
+          font-size: 16px;
+          appearance: textfield;
+          font-weight: 400;
+          outline: none;
+          border-radius: 3px;
+      }
+      h1.logo-wrapper {
+          margin: 0;
+      }
+      header .brand-logo {
+          margin-left: 20px;
+      }
+      nav .brand-logo {
+          position: absolute;
+          color: #fff;
+          display: inline-block;
+          font-size: 2.1rem;
+          padding: 0;
+          white-space: nowrap;
+          height: 0px;
+      }
+      header .brand-logo img {
+          width: 172px;
+          margin-top: 7px;
+      }
+      .notification-badge {
+          position: relative;
+          right: 5px;
+          top: -20px;
+          color: #ffffff;
+          background-color: #FF4081;
+          margin: 0 -.8em;
+          border-radius: 50%;
+          padding: 4px 5px;
+      }
+    `]
+})
+export class HeaderComponent implements  OnInit {
+
+    ngOnInit() {
+
+    }
+}
---
+++ client/src/app/header/header.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        HeaderComponent,
+    ],
+    exports: [
+        HeaderComponent,
+    ]
+})
+export class NavigationModule {
+
+}

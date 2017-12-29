f09f225569d8e34d450a6aca6122dc6c483976fd
Tim <sarrtim@gmail.com>
2017-12-29 13:00:07 +0300

    add assets

---
+++ client/src/app/navigation/navigation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-navigation',
+    templateUrl: 'navigation.component.html'
+})
+export class NavigationComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+}
---
+++ client/src/app/navigation/navigation.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { NavigationComponent } from './navigation.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        NavigationComponent,
+    ],
+    exports: [
+        NavigationComponent,
+    ]
+})
+export class NavigationModule {
+
+}

baa7c5103c547573d22ec5471a7ada9838105d23
Tim <sarrtim@gmail.com>
2017-12-29 13:02:37 +0300

    register imports and declarations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { MaterializeModule } from 'angular2-materialize';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]

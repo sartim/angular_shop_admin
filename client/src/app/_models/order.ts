638ab4690ab88330dd3945d07431a2303bcb9a1f
Tim <sarrtim@gmail.com>
2017-12-30 23:53:19 +0300

    add order model

---
+++ client/src/app/_models/order.ts
@@ -0,0 +1,11 @@
+export class Order {
+    id: string;
+    first_name: string;
+    last_name: string;
+    order_submitted_date: string;
+    created_date:string;
+    restaurant:string;
+    
+    value: string;
+    date: string;
+}

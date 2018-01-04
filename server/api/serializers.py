73a2d7de090f96d7183e6e0bfa6ae0b66d72f64b
Tim <sarrtim@gmail.com>
2018-01-04 12:46:03 +0300

    override create, update & validate on UserSerializer

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -4,9 +4,56 @@ from .models import Category, Product, Order, OrderItem
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    confirm_password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'date_joined',)
+        fields = ('id', 'username', 'email', 'date_joined', 'first_name', 'last_name', 'password',
+                  'confirm_password')
+        read_only_fields = ('date_joined',)
+
+    def create(self, validated_data):
+        user = User.objects.filter(email=validated_data['email'])
+        if user:
+            raise serializers.ValidationError("The email '" +
+                                              validated_data['email'] +
+                                              "' already exists")
+        validated_data.pop('confirm_password')
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name',
+                                                 instance.first_name)
+        instance.last_name = validated_data.get('last_name',
+                                                instance.last_name)
+
+        password = validated_data.get('password', None)
+        confirm_password = validated_data.get('confirm_password', None)
+
+        if password and password == confirm_password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+    def validate(self, data):
+        """
+        Ensure the passwords are the same
+        :param data:
+        :return:
+        """
+        if data['password']:
+            if data['password'] != data['confirm_password']:
+                raise serializers.ValidationError(
+                    "The passwords have to be the same"
+                )
+        return data
 
 
 class CategorySerializer(serializers.ModelSerializer):

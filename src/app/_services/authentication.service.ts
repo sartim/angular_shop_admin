524488f5345d75fcb5ae3ac6762eccd185e668cc
Tim <sarrtim@gmail.com>
2019-03-24 20:21:00 +0300

    Update auth service

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,22 +1,23 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import apiUrl from '../config/api.js';
 
 @Injectable()
 export class AuthenticationService {
     constructor(private http: Http) { }
 
-    login(username: string, password: string) {
-        let body = JSON.stringify({ username: username, password: password });
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
+    login(email: string, password: string) {
+        const body = JSON.stringify({ email: email, password: password });
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        const options = new RequestOptions({ headers: headers });
 
-        let login = this.http.post('https://ordering-api.herokuapp.com/api/v1/auth/token-auth/', body, options)
+        const login = this.http.post(apiUrl + '/account/generate/jwt/', body, options)
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
-                let user = response.json();
-                if (user && user.token) {
+                const user = response.json();
+                if (user && user.access_token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }

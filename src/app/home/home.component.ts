6b8015245fd155d1e236017d246bf2daae186d0a
Tim <sarrtim@gmail.com>
2019-04-24 16:51:26 +0300

    Refactor component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,6 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
 am4core.useTheme(am4themes_animated);
 
-
 @Component({
     moduleId: module.id,
     templateUrl: 'home.component.html',
@@ -23,8 +22,8 @@ export class HomeComponent implements OnInit, OnDestroy {
     currentUser: User;
     users: User[] = [];
     orders: Order[] = [];
-    orders_today: Order[] = [];
-    orders_plot: Order[] = [];
+    ordersToday: Order[] = [];
+    ordersData: Order[] = [];
     dataProvider: any = [];
 
     loggedUser: User;
@@ -51,15 +50,13 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         // this.loadAllUsers();
-        this.fetchUser(); // To fetch logged in user
         this.authenticationService.getNewToken(); // To get new token
         this.authenticationService.getNewTokenHandler(); // To get new token after 2 minutes
         this.loadOrdersToday(); // To get orders today
         this.loadOrdersPlot(); // For orders graph with amcharts plugin
         // this.loadAllDelivered(0); // To get total orders
-        this.orderService.getOrdersPlot().forEach(orders_plot => {
-
-             console.log(orders_plot);
+        this.orderService.getOrdersPlot().forEach(orders => {
+             console.log(orders);
         });
         this.initScript();
     }
@@ -72,7 +69,7 @@ export class HomeComponent implements OnInit, OnDestroy {
             let visits = 10;
             for (let i = 1; i < 366; i++) {
                 visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-                data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+                data.push({ date: new Date(2018, 0, i), name: 'name' + i, value: visits });
             }
             chart.data = data;
             const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
@@ -101,42 +98,30 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
 
     initScript() {
-        // Modify some styles on home entry
         document.body.style.display = 'block';
         document.documentElement.style.display = 'block';
         document.body.style.backgroundColor = '#f9f9f9';
         document.getElementById('header').style.display = 'block';
         document.getElementById('left-sidebar-nav').style.display = 'block';
-        document.getElementById('fab_id').style.display = 'block';
-        // document.getElementById('main').removeAttribute('id');
     }
 
     deleteUser(id: number) {
-      this.userService.delete(id).subscribe(() => { this.loadAllUsers() }, error => { console.log('Error #333'); });
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers(); }, error => { console.log('Error #333'); });
     }
 
     private loadOrdersToday() {
-      const orders_today_ = this.orderService.getOrdersToday();
-      orders_today_.subscribe(orders_today => { this.orders_today = orders_today }, error => {console.log('Error #333');});
+        this.orderService.getOrdersToday().subscribe(orders => { this.ordersToday = orders; }, error => {console.log('Error #333');});
     }
 
     private loadOrders(offset) {
-      this.orderService.getAllOrders(offset).subscribe(orders => { this.orders = orders }, error => { console.log('Error #333'); });
+        this.orderService.getAllOrders(offset).subscribe(orders => { this.orders = orders }, error => { console.log('Error #333'); });
     }
 
-   private loadOrdersPlot() {
-      const orders_plot_ = this.orderService.getOrdersPlot();
-      orders_plot_.subscribe(orders_plot => { this.orders_plot = orders_plot }, error => { console.log('Error #333'); });
-
-    }
-
-    private fetchUser(){
-      const fetch_ = this.userService.fetch();
-      fetch_.subscribe(users => { this.users = users; }, error => { console.log('Error #333'); });
+    private loadOrdersPlot() {
+        this.orderService.getOrdersPlot().subscribe(orders => { this.ordersData = orders; }, error => { console.log('Error #333'); });
     }
 
     private loadAllUsers() {
-      const load_all_ = this.userService.getAll();
-      load_all_.subscribe(users => { this.users = users; }, error => { console.log('Error #333'); });
+        this.userService.getAll().subscribe(users => { this.users = users; }, error => { console.log('Error #333'); });
     }
 }

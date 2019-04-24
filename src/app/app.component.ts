97c77fd8afe545c10303a232fe9c8c655a9cd323
Tim <sarrtim@gmail.com>
2019-04-24 16:15:23 +0300

    Add custom scripts for layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { AlertService, AuthenticationService } from './_services';
+import * as M from '../assets/admin-assets/js/materialize.min';
 
 @Component({
     moduleId: module.id,
@@ -9,10 +10,27 @@ import { AlertService, AuthenticationService } from './_services';
 })
 
 export class AppComponent implements OnInit {
-  constructor(
-        private authenticationService: AuthenticationService) {}
-  ngOnInit() {
-        this.authenticationService.getNewToken(); // To get new token
-        this.authenticationService.getNewTokenHandler(); // To get new token after 2 minutes
+    Collapsible: any;
+    Dropdown: any;
+
+    constructor(private authenticationService: AuthenticationService) {}
+
+    ngOnInit() {
+        // this.authenticationService.getNewToken(); // To get new token
+        // this.authenticationService.getNewTokenHandler(); // To get new token after 2 minutes
+        this.collapsible();
+        this.dropDownTrigger();
+    }
+
+    collapsible() {
+        const elems = document.querySelectorAll('.collapsible');
+        const options = {};
+        const instances = M.Collapsible.init(elems, options);
+    }
+
+    dropDownTrigger() {
+        const elems = document.querySelectorAll('.dropdown-trigger');
+        const options = {};
+        const instances = M.Dropdown.init(elems, options);
     }
 }

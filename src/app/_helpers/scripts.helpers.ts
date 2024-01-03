f1f884618cc25d106ede2e2774162911a20fffa1
Tim <sarrtim@gmail.com>
2024-01-03 20:52:32 +0300

    Add parameter extraction helper

--- src/app/_helpers/scripts.helpers.ts
+++ src/app/_helpers/scripts.helpers.ts
@@ -14,4 +14,15 @@ export class ScriptHelper {
         // @ts-ignore
         document.getElementById('left-sidebar-nav').style.display = 'block';
     }
+
+    getParameterByName(name: string, url: string) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        // tslint:disable-next-line:one-variable-per-declaration
+        const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+
 }

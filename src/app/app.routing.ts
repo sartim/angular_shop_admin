e6549d93e9223e0a2489117cce0735f32c37bd6c
Tim <sarrtim@gmail.com>
2024-01-08 09:10:46 +0300

    Add routes for category component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
 ﻿import { Routes, RouterModule } from '@angular/router';
-
-import { HomeComponent } from './home/index';
-import { OrderListComponent, OrderDetailComponent } from './order/index';
-import { ProductListComponent, ProductDetailComponent } from './product/index';
-import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
-import { AuthGuard } from './_guards/index';
+import { HomeComponent } from './home';
+import { OrderListComponent, OrderDetailComponent } from './order';
+import { ProductListComponent, ProductDetailComponent } from './product';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_guards';
+import { CategoryDetailComponent, CategoryListComponent } from './category';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
@@ -13,6 +13,8 @@ const appRoutes: Routes = [
     { path: 'product-detail/:id', component: ProductDetailComponent, canActivate: [AuthGuard] },
     { path: 'order/all', component: OrderListComponent, canActivate: [AuthGuard] },
     { path: 'order-detail/:id', component: OrderDetailComponent, canActivate: [AuthGuard] },
+    { path: 'category/list', component: CategoryListComponent, canActivate: [AuthGuard] },
+    { path: 'category-detail/:id', component: CategoryDetailComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 

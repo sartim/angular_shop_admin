84eada253f5443c6c42e79bfcafa955d44327d71
Tim <sarrtim@gmail.com>
2024-01-15 08:40:00 +0300

    Pagination

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,5 +1,5 @@
 import {ActivatedRoute, Router} from '@angular/router';
-import {AfterViewInit, Component, OnDestroy, OnInit, Renderer2} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2} from '@angular/core';
 import {Order, User} from '../_models';
 import {AlertService, AuthenticationService, ProductService} from '../_services';
 import { Product } from '../_models';
@@ -33,9 +33,11 @@ export class ProductListComponent implements AfterViewInit, OnInit, OnDestroy {
     endEntry = 0;
     totalEntries = 0;
     entryPoint = 1;
+    pages!: string;
     private f = 0;
 
     constructor(
+        private elementRef: ElementRef,
         private alertService: AlertService,
         private authenticationService: AuthenticationService,
         private route: ActivatedRoute,
@@ -94,6 +96,7 @@ export class ProductListComponent implements AfterViewInit, OnInit, OnDestroy {
     private loadAll(page: number, startEntry: number, endEntry: number) {
         this.loading = true;
         this.previous = false;
+        this.products = new Product();
         const loadAll = this.productService.getProducts(page);
         loadAll.subscribe((products: Product) => {
                 this.products = products;
@@ -109,6 +112,23 @@ export class ProductListComponent implements AfterViewInit, OnInit, OnDestroy {
                 this.totalEntries = this.products.count;
                 this.loading = false;
                 this.navigation = true;
+                let str = '';
+                const totalPages = Math.ceil(this.totalEntries / 20);
+                const pagesToShow = 5;
+                const halfPagesToShow = Math.floor(pagesToShow / 2);
+                let startPage = Math.max(1, page - halfPagesToShow);
+                const endPage = Math.min(totalPages, startPage + pagesToShow - 1);
+                if (endPage - startPage + 1 < pagesToShow) {
+                  startPage = Math.max(1, endPage - pagesToShow + 1);
+                }
+                for (let i = startPage; i <= endPage; i++) {
+                  if (page === i) {
+                    str += '<li class="active waves-effect"><a class="current_page">' + i + '</a></li>';
+                  } else {
+                    str += '<li class="waves-effect"><a>' + i + '</a></li>';
+                  }
+                }
+                this.pages = str;
             },
             (error) => {
                 this.loading = false;

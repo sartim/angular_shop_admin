9532003f8c1ced89e3a87a171c1a7b086bdce9d3
Tim <sarrtim@gmail.com>
2024-03-12 22:23:15 +0300

    Modify category service to inherit from base service class

--- src/app/_services/category.service.ts
+++ src/app/_services/category.service.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from './authentication.service';
-import {HttpClient} from '@angular/common/http';
-import {Category, CategoryDetail} from '../_models';
-// @ts-ignore
-import apiUrl from '../config/api.js';
+import { BaseService } from './base.service';
+import { Category, CategoryDetail } from '../_models';
 
 @Injectable()
-export class  CategoryService {
-    constructor(private http: HttpClient, private authService: AuthenticationService, ) { }
-
-    getCategories(page: number) {
-        return this.http.get<Category>(apiUrl +'/api/v1/categories' + '?page=' + page, this.authService.jwt());
-    }
-
-    getCategoryById(id: string | null) {
-        return this.http.get<CategoryDetail>(apiUrl + '/api/v1/categories/' + id, this.authService.jwt());
-    }
+export class CategoryService extends BaseService<Category, CategoryDetail> {
+  constructor(http: HttpClient, authService: AuthenticationService) {
+    super(http, authService, 'api/v1/categories');
+  }
 }

8ac9059fda2d794f61a720e0b9a243e7f3fa7899
Tim <sarrtim@gmail.com>
2024-03-17 23:10:21 +0300

    Update order.service.ts

--- src/app/_services/order.service.ts
+++ src/app/_services/order.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from './authentication.service';
 import { BaseService } from './base.service';
 import { Order, OrderDetail } from '../_models';
-import apiUrl from '../config/api';
 
 @Injectable()
 export class OrderService extends BaseService<Order, OrderDetail> {
@@ -11,26 +10,26 @@ export class OrderService extends BaseService<Order, OrderDetail> {
     super(http, authService, 'api/v1/orders');
   }
 
-  getOrdersToday() {
+  getOrdersToday(url: string) {
       // @ts-ignore
-      return this.http.get<Order>(apiUrl + '/api/v1/orders/today', this.authService.jwt());
+      return this.http.get<Order>(url, this.authService.jwt());
   }
 
-  getOrdersThisMonth() {
+  getOrdersThisMonth(url: string) {
       // @ts-ignore
       // tslint:disable-next-line:max-line-length
-      return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
+      return this.http.get<Order>(url, this.authService.jwt());
   }
 
-  getOrdersLastMonth() {
+  getOrdersLastMonth(url: string) {
       // @ts-ignore
       // tslint:disable-next-line:max-line-length
-      return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
+      return this.http.get<Order>(url, this.authService.jwt());
   }
 
-  getOrdersPlot() {
+  getOrdersPlot(url: string) {
       // @ts-ignore
       // tslint:disable-next-line:max-line-length
-      return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
+      return this.http.get<Order>(url, this.authService.jwt());
   }
 }

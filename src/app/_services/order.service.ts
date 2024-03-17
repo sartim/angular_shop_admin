f671349474290b94c49bf77b0ff3b41009177ba4
Tim <sarrtim@gmail.com>
2024-03-17 21:56:16 +0300

    Inherit from base service on order.service.ts

--- src/app/_services/order.service.ts
+++ src/app/_services/order.service.ts
@@ -1,45 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from './authentication.service';
-import {HttpClient} from '@angular/common/http';
-import {Order, OrderDetail} from '../_models';
-// @ts-ignore
-import apiUrl from '../config/api.js';
+import { BaseService } from './base.service';
+import { Order, OrderDetail } from '../_models';
+import apiUrl from '../config/api';
 
 @Injectable()
-export class OrderService {
-    constructor(private http: HttpClient, private authService: AuthenticationService, ) { }
+export class OrderService extends BaseService<Order, OrderDetail> {
+  constructor(http: HttpClient, authService: AuthenticationService) {
+    super(http, authService, 'api/v1/orders');
+  }
 
-    getOrdersToday() {
-        // @ts-ignore
-        return this.http.get<Order>(apiUrl + '/api/v1/orders/today', this.authService.jwt());
-    }
+  getOrdersToday() {
+      // @ts-ignore
+      return this.http.get<Order>(apiUrl + '/api/v1/orders/today', this.authService.jwt());
+  }
 
-    getOrdersThisMonth() {
-        // @ts-ignore
-        // tslint:disable-next-line:max-line-length
-        return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
-    }
+  getOrdersThisMonth() {
+      // @ts-ignore
+      // tslint:disable-next-line:max-line-length
+      return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
+  }
 
-    getOrdersLastMonth() {
-        // @ts-ignore
-        // tslint:disable-next-line:max-line-length
-        return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
-    }
+  getOrdersLastMonth() {
+      // @ts-ignore
+      // tslint:disable-next-line:max-line-length
+      return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
+  }
 
-    getOrdersPlot() {
-        // @ts-ignore
-        // tslint:disable-next-line:max-line-length
-        return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
-    }
-
-    getAllOrders(page: number) {
-        // @ts-ignore
-        return this.http.get<Order>(apiUrl + '/api/v1/orders' + '?page=' + page, this.authService.jwt());
-    }
-
-    getOrderById(id: number | null) {
-        // @ts-ignore
-        // tslint:disable-next-line:max-line-length
-        return this.http.get<OrderDetail>(apiUrl + '/api/v1/orders/' + id, this.authService.jwt());
-    }
+  getOrdersPlot() {
+      // @ts-ignore
+      // tslint:disable-next-line:max-line-length
+      return this.http.get<Order>(apiUrl + '/api/v1/orders', this.authService.jwt());
+  }
 }

f75f4f26cce8aba1508f0f3e27ef5b19599ebdd9
Tim <sarrtim@gmail.com>
2024-03-20 11:47:09 +0300

    Add date fields

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -24,4 +24,8 @@ export class UserDetail {
         full_name: string,
         id: string
     };
+    // tslint:disable-next-line:variable-name
+    created_at!: string;
+    // tslint:disable-next-line:variable-name
+    updated_at!: string;
 }
